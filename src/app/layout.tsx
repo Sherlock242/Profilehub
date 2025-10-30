23ff629cfbd627db51719e68b91f6198e23b5ba1
Santosh Kanojiya <kanojiyas780@gmail.com>
2025-10-30 18:49:21 +0000

    Nothing should overflow outside screen, and change theme to black backgr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className="h-full dark">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
@@ -24,7 +24,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="font-body antialiased h-full">
+      <body className="font-body antialiased h-full overflow-x-hidden">
         <AuthProvider>
           <div className="flex flex-col min-h-screen">
             <Header />
