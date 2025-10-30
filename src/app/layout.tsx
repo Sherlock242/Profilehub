fca5509c100468150767b868b688de4dfe65e77c
Santosh Kanojiya <kanojiyas780@gmail.com>
2025-10-30 22:10:06 +0000

    Style profilehub font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default async function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Lexend:wght@700&display=swap"
           rel="stylesheet"
         />
       </head>
