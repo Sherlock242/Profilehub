530394fb9e88eb7f9431b63dbe63932fe31123a0
Santosh Kanojiya <kanojiyas780@gmail.com>
2025-10-30 22:18:24 +0000

    Use capital letter for profilehub

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export function Logo() {
   return (
     <Link href="/" className="flex items-center" prefetch={false}>
-      <span className="text-lg font-bold font-headline tracking-tighter">
+      <span className="text-lg font-bold font-headline tracking-tighter uppercase">
         <span className="text-accent">Profile</span>
         <span>Hub</span>
       </span>
