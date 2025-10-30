f06c38c9306fb1cf1c14d615088dff3f1d2de5e2
Santosh Kanojiya <kanojiyas780@gmail.com>
2025-10-30 22:16:35 +0000

    Give header margin of 5 px right and left

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -34,7 +34,7 @@ export function Header({ user }: { user: AppUser | null }) {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center">
+      <div className="container flex h-16 items-center px-[5px]">
         <Logo />
         <div className="flex flex-1 items-center justify-end space-x-2">
             {user ? (
